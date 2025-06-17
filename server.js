2571cb7d2fad633090b1b047d9e0261bea718e3a
Raj <rajbaidyanath76@gmail.com>
2025-06-17 20:11:06 +0530

    mongo cnct backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();  //imported the env file
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
